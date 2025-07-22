b19abdae8401b12b05b5d614d910da1cc10c2ff8
Layon Walker <layon.pedro@estudante.ufla.br>
2025-07-22 20:08:28 -0300

    tela de recover feita

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
 import LoginPage from './pages/Login.jsx';
 import RegisterPage from './pages/Register.jsx';
-// import RecoverPassPage from './pages/RecoverPass.jsx';
+import RecoverPassPage from './pages/RecoverPass.jsx';
 
 const router = createBrowserRouter([
   {
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/login", element: <LoginPage /> },
       { path: "/", element: <RegisterPage /> },
-      // { path: "/recover-password", element: <RecoverPassPage /> },
+      { path: "/recover-password", element: <RecoverPassPage /> },
     ],
   },
 ]);
--- src/pages/RecoverPass.jsx
+++ src/pages/RecoverPass.jsx
@@ -1,10 +1,42 @@
-
+import styles from "./RecoverPass.module.css";
 import AuthLayout from "../components/layout/AuthLayout";
+import InputCommon from "../components/common/Input";
+import ButtonCommon from "../components/common/Button";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 function RecoverPassPage() {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("tentativa de recuperação de senha:", { email });
+  };
+
   return (
     <AuthLayout>
-      <h1>Página de Recuperação de Senha</h1>
+      <div className={styles.recoverContainer}>
+        <div className={styles.mainContent}>
+          <h2 className={styles.title}>Recuperar Senha</h2>
+          <form className={styles.form} onSubmit={handleSubmit}>
+            <InputCommon
+              type="email"
+              placeholder="E-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <ButtonCommon type="submit">
+              <ArrowForwardIcon fontSize="large" />
+            </ButtonCommon>
+          </form>
+        </div>
+
+        <div className={styles.links}>
+          <Link to="/">Criar Conta</Link>
+          <Link to="/login">Fazer Login</Link>
+        </div>
+      </div>
     </AuthLayout>
   );
 }
