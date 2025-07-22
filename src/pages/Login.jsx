052bf586c13eb993a71467ac3b06afa5eec03594
Layon Walker <layon.pedro@estudante.ufla.br>
2025-07-21 22:48:59 -0300

    tela de login e componentes feitos

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,50 @@
+// LoginPage.jsx - Nova Estrutura
 
 import styles from './Login.module.css';
 import AuthLayout from '../components/layout/AuthLayout';
+import Input from '../components/common/Input';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa';
 
 function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('tentativa de login:', { email, password });
+  };
+
   return (
     <AuthLayout>
-      <div className={styles.loginForm}>
-        <h2 className={styles.title}>Login</h2>
-        <form>
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Senha" />
-          <button type="submit">→</button>
-        </form>
+      <div className={styles.loginContainer}>
+
+        <div className={styles.mainContent}>
+          <h2 className={styles.title}>Login</h2>
+          <form className={styles.form} onSubmit={handleSubmit}>
+            <Input 
+              type="email" 
+              placeholder="E-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input 
+              type="password" 
+              placeholder="Senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit" className={styles.submitButton}><FaArrowRight /></button>
+
+          </form>
+        </div>
+        
         <div className={styles.links}>
-            <a href="/register">Registrar</a>
-            <a href="/recover-password">Esqueci minha senha</a>
+          <Link to="/register">Criar Conta</Link>
+          <Link to="/recover-password">Esqueceu a Senha?</Link>
         </div>
       </div>
-
     </AuthLayout>
   );
 }
