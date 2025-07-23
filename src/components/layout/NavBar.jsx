6b90880c4f8a856b6980f798137b09576cf3401e
Layon Walker <layon.pedro@estudante.ufla.br>
2025-07-23 20:49:42 -0300

    Componente NavBar feita

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,21 +1,30 @@
+
+import { Link } from 'react-router-dom';
+import styles from './NavBar.module.css';
 import icone from '../../assets/images/perfil_icone.png'; 
+import SearchIcon from '@mui/icons-material/Search'; 
 
 function NavBar() {
   return (
-    <nav>
-        <ul>
-        <img src={icone} alt="Icone de Perfil" />
-        <li><a href="/launches">Lançamentos</a></li>
-        <li><a href="/popular">Populares</a></li>
-        <li><a href="/genres">Gêneros</a></li>
-        <li><a href="/promotions">Promoções</a></li>
-        <li><a href="/account">Conta</a></li>
-        </ul>
-          
-        <div className="search-bar">
-            <input type="text" placeholder="Pesquisar jogos..." />
-            <button type="submit">Buscar</button>
-        </div>
+    <nav className={styles.navContainer}>
+      <Link to="/login" className={styles.logo}>
+        <img src={icone} alt="Logotipo Game Dame" />
+      </Link>
+
+      <ul className={styles.navList}>
+        <li><Link to="/launches">Lançamentos</Link></li>
+        <li><Link to="/popular">Populares</Link></li>
+        <li><Link to="/genres">Gêneros</Link></li>
+        <li><Link to="/promotions">Promoções</Link></li>
+        <li><Link to="/account">Conta</Link></li>
+      </ul>
+
+      <div className={styles.searchBar}>
+        <input type="text" placeholder="" />
+        <button type="submit" aria-label="Buscar">
+          <SearchIcon fontSize="small" />
+        </button>
+      </div>
     </nav>
   );
 }
